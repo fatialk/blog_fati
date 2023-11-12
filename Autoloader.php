014e6df8da78569d070dbf6645ed9e4b32992789
utilisateur <elachri.fz@gmail.com>
2023-11-12 13:35:14 +0100

    fix href cv

--- Autoloader.php
+++ Autoloader.php
@@ -1,18 +1,17 @@
 <?php
+
 namespace App;
+
 class Autoloader
 {
     public static function register()
     {
         spl_autoload_register(
-            function($className) {
+            function ($className) {
                 $fileName = __DIR__ . '/src/' .  str_replace('App\\', '/', $className) . ".php";
-                if(file_exists($fileName))
-                {
+                if (file_exists($fileName)) {
                     require_once($fileName);
-                }
-                else
-                {
+                } else {
                     echo htmlspecialchars("$fileName not found<br>\n", ENT_HTML5);
                 }
             }
